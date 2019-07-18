7551674eb1a349ef4b57f4d9becfa441ee32c6db
Kevin Chow <kevinchow@telus.net>
2019-07-18 10:44:52 -0700

    Forgot to commit smart-tabs-mode initialization

---
+++ .emacs.d/lisp/frontend-packages/init-smart-tabs-mode.el
@@ -0,0 +1,12 @@
+;;; init-smart-tabs-mode.el --- Initialise SmartTabs
+
+;;; Commentary:
+
+;;; Code:
+(use-package smart-tabs-mode
+  :ensure t
+  :config
+  (smart-tabs-insinuate 'c))
+
+(provide 'init-smart-tabs-mode)
+;;; init-smart-tabs-mode.el ends here
