9c3266129867ac0c36374929751d57662d58296b
Kevin Chow <kevinchow@telus.net>
2020-09-30 15:41:40 -0700

    Accidental commit of autogenerated code

--- .emacs.d/lisp/backend-packages/init-ccls-exec.el
+++
@@ -1,8 +0,0 @@
-;;; init-ccls-exec.el : Set ccls executable
-;;; This file will be autocompleted on setup.
-;;; example:
-;;;   (setq ccls-executable "$HOME/path/to/ccls/executable/ccls")
-
-;; Begin generated code:
-(setq ccls-executable "/home/chowkevi/Documents/dev/tools/ccls/Release/ccls")
-(provide 'init-ccls-exec)
