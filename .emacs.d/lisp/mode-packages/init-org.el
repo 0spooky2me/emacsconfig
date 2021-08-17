c8ce4c0a73c48f4cd192ed56b243a915da320a67
Kevin Chow <kevinchow@telus.net>
2021-08-17 11:26:48 -0700

    fix org and windmove keybinding conflict

--- .emacs.d/lisp/mode-packages/init-org.el
+++ .emacs.d/lisp/mode-packages/init-org.el
@@ -1,11 +1,21 @@
-;; init-org.el : Initialize and config org-mode
+;;; init-org.el --- Initialize and config org-mode
+
+;;; Commentary:
+
+;;; Code:
+(use-package org
+  :ensure t
+  ;; windmove conflicts with org, so fix hooks
+  :hook ((org-shiftup-final . windmove-up)
+		 (org-shiftleft-final . windmove-left)
+		 (org-shiftdown-final . windmove-down)
+		 (org-shiftright-final . windmove-right)))
 
 (use-package org-bullets
   :ensure t
-  :config
-  (setq org-bullets-bullet-list
-	'("◉" "◎" "⚫" "○" "✪" "◇"))
-  :hook
-  (org-mode . org-bullets-mode))
+  :after (org)
+  :config (setq org-bullets-bullet-list '("◉" "◎" "⚫" "○" "✪" "◇"))
+  :hook (org-mode . org-bullets-mode))
 
 (provide 'init-org)
+;;; init-org.el ends here
